81948bcc26b41bad44a5931f4e2eae18fcb0a293
lucasivan7 <lucasivancaballero@hotmail.com>
2023-03-10 18:07:33 -0300

    logica para la toma de la ubicacio y crracion de mansaje

--- mensaje.py
+++ mensaje.py
@@ -1,26 +1,36 @@
 from twilio.rest import Client
 from flask import request
+import os
+from dotenv import load_dotenv
+
+# import credentials dotenv
+load_dotenv()
+
 
 # Datos de la cuenta de Twilio
-account_sid = 'AC702d052c1ddbfdef80e6d43e902129cd'
-auth_token = '104a39c7a1546d55b505498024b458e6'
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 
-def enviar_ubicacion():
+def enviar_ubicacion(lat, long):
     print("Enviando ubicación...")
-    # Obtener la ubicación del dispositivo a través de request.args.get()
-    latitud = request.args.get('latitud')
-    longitud = request.args.get('longitud')
-
     # Construir el mensaje de ubicación
-    ubicacion = "geo:{},{}".format(latitud, longitud)
+    ubicacion = "geo:{},{}".format(lat, long)
     print(ubicacion)
+    # crear objeto mensaje
+    mensaje = {
+        "body":"Aqui estoy!",
+        "location": {
+            "latitud": lat,
+            "longitud": long
+        }
+    
+    }
     # Enviar el mensaje de ubicación a través de Twilio
     message = client.messages.create(
+        body=f"Estoy aqui bro: https://www.google.com/maps/search/?api=1&query={lat},{long}",
         from_='+15672352765',
-        to='+595994256673',
-        body='Mi ubicación actual:'
-        # , media_url=ubicacion
+        to='+595994256673'
     )
 
     # Imprimir la respuesta de Twilio
